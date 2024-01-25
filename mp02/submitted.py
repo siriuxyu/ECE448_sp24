08b28fd23e15ff53b0d722463cfca7a08f08a635
Siriuxxx <hengjia.y@outlook.com>
2024-01-25 06:09:53 -0600

    0124 mp2 finished

--- mp02/submitted.py
+++ mp02/submitted.py
@@ -29,7 +29,19 @@ def create_frequency_table(train):
         - frequency[y][x] = number of occurrences of bigram x in texts of class y,
           where x is in the format 'word1*-*-*-*word2'
     '''
-    raise RuntimeError("You need to write this part!")
+    freq_list = {}
+    frequency = {}
+    for i in train.keys():
+        freq_list[i] = []
+        for j in range(len(train[i])):
+            train[i][j][0] = train[i][j][0].lower()
+            for k in range(len(train[i][j]) - 1):
+                train[i][j][k + 1] = train[i][j][k + 1].lower()
+                freq_list[i].append(train[i][j][k] + '*-*-*-*' + train[i][j][k + 1])
+        frequency[i] = Counter(freq_list[i])
+
+    return frequency
+    # raise RuntimeError("You need to write this part!")
 
 def remove_stopwords(frequency):
     '''
@@ -45,7 +57,15 @@ def remove_stopwords(frequency):
         - nonstop[y][x] = frequency of bigram x in texts of class y,
           but only if neither token in x is a stopword. x is in the format 'word1*-*-*-*word2'
     '''
-    raise RuntimeError("You need to write this part!")
+    nonstop = {}
+    for i in frequency.keys():
+        nonstop[i] = Counter()
+        for j in frequency[i].keys():
+            if j.split('*-*-*-*')[0] not in stopwords or j.split('*-*-*-*')[1] not in stopwords:
+                nonstop[i][j] = frequency[i][j]
+                
+    return nonstop
+    # raise RuntimeError("You need to write this part!")
 
 
 def laplace_smoothing(nonstop, smoothness):
@@ -68,7 +88,18 @@ def laplace_smoothing(nonstop, smoothness):
     Be careful that your vocabulary only counts bigrams that occurred at least once
     in the training data for class y.
     '''
-    raise RuntimeError("You need to write this part!")
+    likelihood = {}
+    for i in nonstop.keys():
+        likelihood[i] = {}
+        # likelihood[i]['OOV'] = 0
+        sum_bigram = sum(nonstop[i].values())
+        for j in nonstop[i].keys():
+            likelihood[i][j] = (nonstop[i][j] + smoothness) / (sum_bigram + smoothness * (len(nonstop[i].keys()) + 1))
+        likelihood[i]['OOV'] = smoothness / (sum_bigram + smoothness * (len(nonstop[i].keys()) + 1))
+        
+    return likelihood
+    
+    # raise RuntimeError("You need to write this part!")
 
 def naive_bayes(texts, likelihood, prior):
     '''
@@ -85,7 +116,33 @@ def naive_bayes(texts, likelihood, prior):
     hypotheses (list)
         - hypotheses[i] = class label for the i'th text
     '''
-    raise RuntimeError("You need to write this part!")
+    hypotheses = list(range(len(texts)))
+    for i in range(len(texts)):
+        likelihood_pos = np.log(prior)
+        likelihood_neg = np.log(1 - prior)
+        texts[i][0] = texts[i][0].lower()
+        for j in range(len(texts[i]) - 1):
+            texts[i][j + 1] = texts[i][j + 1].lower()
+            if (texts[i][j] in stopwords) and (texts[i][j + 1] in stopwords):
+                continue
+            temp_key = texts[i][j] + '*-*-*-*' + texts[i][j + 1]
+            if temp_key not in likelihood['pos'].keys():
+                likelihood_pos += np.log(likelihood['pos']['OOV'])
+            else:
+                likelihood_pos += np.log(likelihood['pos'][temp_key])
+            if temp_key not in likelihood['neg'].keys():
+                likelihood_neg += np.log(likelihood['neg']['OOV'])
+            else:
+                likelihood_neg += np.log(likelihood['neg'][temp_key])
+        if likelihood_pos > likelihood_neg:
+            hypotheses[i] = ('pos')
+        elif likelihood_pos < likelihood_neg:
+            hypotheses[i] = ('neg')
+        else:
+            hypotheses[i] = ('undecided')
+    
+    return hypotheses
+    # raise RuntimeError("You need to write this part!")
 
 
 
@@ -108,5 +165,15 @@ def optimize_hyperparameters(texts, labels, nonstop, priors, smoothnesses):
         - accuracies[m,n] = dev set accuracy achieved using the
           m'th candidate prior and the n'th candidate smoothness
     '''
-    raise RuntimeError("You need to write this part!")
+    accuracies = np.zeros((len(priors), len(smoothnesses)))
+    for i in range(len(priors)):
+        for j in range(len(smoothnesses)):
+            likelihood = laplace_smoothing(nonstop, smoothnesses[j])
+            hypotheses = naive_bayes(texts, likelihood, priors[i])
+            for k in range(len(hypotheses)):
+                if hypotheses[k] == labels[k]:
+                    accuracies[i,j] += 1
+            accuracies[i,j] /= len(hypotheses)
+    return accuracies
+    # raise RuntimeError("You need to write this part!")
                           
