85e109c5c68a14de15d8afbba1aeff5684e05129
Siriuxxx <hengjia.y@outlook.com>
2024-01-29 15:40:49 -0600

    mp03 orginal files

---
+++ mp03/submitted.py
@@ -0,0 +1,53 @@
+'''
+This is the module you'll submit to the autograder.
+
+There are several function definitions, here, that raise RuntimeErrors.  You should replace
+each "raise RuntimeError" line with a line that performs the function specified in the
+function's docstring.
+
+For implementation of this MP, You may use numpy (though it's not needed). You may not 
+use other non-standard modules (including nltk). Some modules that might be helpful are 
+already imported for you.
+'''
+
+import math
+from collections import defaultdict, Counter
+from math import log
+import numpy as np
+
+# define your epsilon for laplace smoothing here
+
+def baseline(test, train):
+    '''
+    Implementation for the baseline tagger.
+    input:  test data (list of sentences, no tags on the words, use utils.strip_tags to remove tags from data)
+            training data (list of sentences, with tags on the words)
+    output: list of sentences, each sentence is a list of (word,tag) pairs.
+            E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
+    '''
+    raise NotImplementedError("You need to write this part!")
+
+
+def viterbi(test, train):
+    '''
+    Implementation for the viterbi tagger.
+    input:  test data (list of sentences, no tags on the words)
+            training data (list of sentences, with tags on the words)
+    output: list of sentences with tags on the words
+            E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
+    '''
+    raise NotImplementedError("You need to write this part!")
+
+
+def viterbi_ec(test, train):
+    '''
+    Implementation for the improved viterbi tagger.
+    input:  test data (list of sentences, no tags on the words). E.g.,  [[word1, word2], [word3, word4]]
+            training data (list of sentences, with tags on the words). E.g.,  [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
+    output: list of sentences, each sentence is a list of (word,tag) pairs.
+            E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
+    '''
+    raise NotImplementedError("You need to write this part!")
+
+
+
