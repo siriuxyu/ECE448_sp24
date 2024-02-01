466ad12bbb6e91812273a18f1db9ca0c81f4e20e
Siriuxxx <hengjia.y@outlook.com>
2024-01-31 19:47:25 -0600

    0131 mp03 finished w/ extra

--- mp03/submitted.py
+++ mp03/submitted.py
@@ -25,7 +25,6 @@ def baseline(test, train):
     output: list of sentences, each sentence is a list of (word,tag) pairs.
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    import utils
     word_tag_pairs = Counter()
     for i in range(len(train)):
         for j in range(len(train[i])):
@@ -63,29 +62,32 @@ def viterbi(test, train):
     output: list of sentences with tags on the words
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    tags = []
-    initial_tags = []
+    smoothness = 0.0001     # laplace smoothness
+    tags = []               # list of tags
+    # initial_tags = []
     for i in range(len(train)):
         for j in range(len(train[i])):
-            tag = train[i][j][1]
+            tag = train[i][j][1]        # collect all tags (no duplicates)
             if tag not in tags:
                 tags.append(train[i][j][1])
-                initial_tags.append(0)
+                # initial_tags.append(0)
         initial_tag = train[i][0][1]
-        initial_tags[tags.index(initial_tag)] += 1
-    initial_probability = np.array(initial_tags) / sum(initial_tags)
+        # initial_tags[tags.index(initial_tag)] += 1
+    # initial_probability = np.array(initial_tags) / sum(initial_tags)
     
     # compute transition probability
+    transition_count = np.zeros((len(tags), len(tags)))
     transition_probability = np.zeros((len(tags), len(tags)))
     for i in range(len(train)):
         for j in range(len(train[i]) - 1):
-            transition_probability[tags.index(train[i][j][1])][tags.index(train[i][j + 1][1])] += 1
+            transition_count[tags.index(train[i][j][1])][tags.index(train[i][j + 1][1])] += 1
 
-    for i in range(len(transition_probability)):
-        transition_probability[i] = transition_probability[i] / sum(transition_probability[i])
+    for i in range(len(transition_probability)):    # Laplace smoothing
+        tag_sum = sum(transition_count[i])
+        for j in range(len(transition_probability[i])):
+            transition_probability[i][j] = (transition_count[i][j] + smoothness) / (tag_sum + smoothness * len(tags))
     
     # compute emission probability
-    smoothness = 0.0001
     emission_count = []
     emission_probability = []
     for i in range(len(tags)):
@@ -101,30 +103,39 @@ def viterbi(test, train):
         emission_probability[i]['OOV'] = smoothness / (tag_sum + smoothness * (len(emission_count[i].keys()) + 1))
             
     # viterbi algorithm
-    for i in range(len(test)):
-        viterbi_matrix = np.zeros((len(tags), len(test[i])))
-        backpointer = np.zeros((len(tags), len(test[i])))
-        for j in range(len(test[i])):
-            if j == 0:
-                for k in range(len(tags)):
-                    if test[i][j] in emission_probability[k].keys():
-                        viterbi_matrix[k][j] = np.log(initial_probability[k]) + np.log(emission_probability[k][test[i][j]])
-                    else:
-                        viterbi_matrix[k][j] = np.log(initial_probability[k]) + np.log(emission_probability[k]['OOV'])
-            for k in range(len(tags)):
-                if test[i][j] in emission_probability[k].keys():
-                    temp = np.log(viterbi_matrix[:, j - 1]) + np.log(transition_probability[:, k]) + np.log(emission_probability[k][test[i][j]])
-                else:
-                    temp = np.log(viterbi_matrix[:, j - 1]) + np.log(transition_probability[:, k]) + np.log(emission_probability[k]['OOV'])
-                viterbi_matrix[k][j] = max(temp)
-                backpointer[k][j] = np.argmax(temp)
     result = []
     for i in range(len(test)):
-        temp = []
-        for j in range(len(test[i])):
-            temp.append((test[i][j], tags[int(backpointer[:, j][-1])]))
-        result.append(temp)
+        temp_result = []
+        temp_col = np.zeros(len(tags))
+        temp_result.append(('END', 'END'))
+        viterbi_matrix = np.zeros((len(tags), len(test[i])))
+        backpointer = np.zeros((len(tags), len(test[i])))
+        for k in range(len(tags)):
+            if tags[k] == 'START':
+                viterbi_matrix[k][0] = np.log((1+smoothness)/(1+smoothness*len(tags)))
+            else:
+                viterbi_matrix[k][0] = np.log(smoothness/(1+smoothness*len(tags)))
+        for j in range(1,len(test[i])-1):
+            # initial probability
+            for k in range(len(tags)):
+                if test[i][j] in emission_probability[k].keys():
+                    temp_col = viterbi_matrix[:, j - 1] + np.log(transition_probability[:, k]) + np.log(emission_probability[k][test[i][j]])
+                else:
+                    temp_col = viterbi_matrix[:, j - 1] + np.log(transition_probability[:, k]) + np.log(emission_probability[k]['OOV'])
+                viterbi_matrix[k][j] = max(temp_col)
+                backpointer[k][j] = np.argmax(temp_col)
+        # find the most possible tag for the last word, then trace back
+        
+        temp_result.append((test[i][len(test[i])-2], tags[np.argmax(viterbi_matrix[:, len(test[i])-2])]))
+        temp_tag_index = backpointer[np.argmax(viterbi_matrix[:, len(test[i])-2])][len(test[i])-2]
+        for j in range(len(test[i])-3, 0,-1):
+            temp_result.append((test[i][j], tags[int(temp_tag_index)]))
+            temp_tag_index = backpointer[int(temp_tag_index)][j]
+        temp_result.append((test[i][0], 'START'))
+        temp_result.reverse()
+        result.append(temp_result)
     return result
+
     # raise NotImplementedError("You need to write this part!")
 
 
@@ -136,6 +147,94 @@ def viterbi_ec(test, train):
     output: list of sentences, each sentence is a list of (word,tag) pairs.
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
+    smoothness = 0.00001     # laplace smoothness
+    tags = []               # list of tags
+    words_count = Counter()
+    hapax_tags = {}
+    # initial_tags = []
+    for i in range(len(train)):
+        for j in range(len(train[i])):
+            tag = train[i][j][1]        # collect all tags (no duplicates)
+            # collect all hapax tags
+            words_count[train[i][j][0]] += 1
+            if words_count[train[i][j][0]] == 1:
+                hapax_tags[train[i][j][0]] = tag
+            else:       # remove hapax tags
+                if train[i][j][0] in hapax_tags.keys():
+                    del hapax_tags[train[i][j][0]]
+            if tag not in tags:
+                tags.append(train[i][j][1])
+
+    # compute P(T|hapax) for each hapax tag
+    count_T_hapax = [0] * len(tags)
+    for i in hapax_tags.values():
+        if i in tags:
+            count_T_hapax[tags.index(i)] += 1
+    # laplace smoothing
+    P_T_hapax = (np.array(count_T_hapax) + smoothness) / (sum(count_T_hapax) + smoothness * len(tags))
+    
+    # compute transition probability
+    transition_count = np.zeros((len(tags), len(tags)))
+    transition_probability = np.zeros((len(tags), len(tags)))
+    for i in range(len(train)):
+        for j in range(len(train[i]) - 1):
+            transition_count[tags.index(train[i][j][1])][tags.index(train[i][j + 1][1])] += 1
+
+    for i in range(len(transition_probability)):    # Laplace smoothing
+        tag_sum = sum(transition_count[i])
+        for j in range(len(transition_probability[i])):
+            transition_probability[i][j] = (transition_count[i][j] + smoothness) / (tag_sum + smoothness * len(tags))
+    
+    # compute emission probability
+    emission_count = []
+    emission_probability = []
+    for i in range(len(tags)):
+        emission_count.append(Counter())
+        emission_probability.append(Counter())
+    for i in range(len(train)):
+        for j in range(len(train[i])):
+            emission_count[tags.index(train[i][j][1])][train[i][j][0]] += 1
+    for i in range(len(emission_count)):
+        temp_smoothness = smoothness * P_T_hapax[i]
+        tag_sum = sum(emission_count[i].values())
+        for j in emission_count[i].keys():
+            emission_probability[i][j] = (emission_count[i][j] + temp_smoothness) / (tag_sum + temp_smoothness * (len(emission_count[i].keys()) + 1))
+        emission_probability[i]['OOV'] = temp_smoothness / (tag_sum + temp_smoothness * (len(emission_count[i].keys()) + 1))
+            
+    # viterbi algorithm
+    result = []
+    for i in range(len(test)):
+        temp_result = []
+        temp_col = np.zeros(len(tags))
+        temp_result.append(('END', 'END'))
+        viterbi_matrix = np.zeros((len(tags), len(test[i])))
+        backpointer = np.zeros((len(tags), len(test[i])))
+        for k in range(len(tags)):
+            if tags[k] == 'START':
+                viterbi_matrix[k][0] = np.log((1+smoothness)/(1+smoothness*len(tags)))
+            else:
+                viterbi_matrix[k][0] = np.log(smoothness/(1+smoothness*len(tags)))
+        for j in range(1,len(test[i])-1):
+            # initial probability
+            for k in range(len(tags)):
+                if test[i][j] in emission_probability[k].keys():
+                    temp_col = viterbi_matrix[:, j - 1] + np.log(transition_probability[:, k]) + np.log(emission_probability[k][test[i][j]])
+                else:
+                    temp_col = viterbi_matrix[:, j - 1] + np.log(transition_probability[:, k]) + np.log(emission_probability[k]['OOV'])
+                viterbi_matrix[k][j] = max(temp_col)
+                backpointer[k][j] = np.argmax(temp_col)
+        # find the most possible tag for the last word, then trace back
+        
+        temp_result.append((test[i][len(test[i])-2], tags[np.argmax(viterbi_matrix[:, len(test[i])-2])]))
+        temp_tag_index = backpointer[np.argmax(viterbi_matrix[:, len(test[i])-2])][len(test[i])-2]
+        for j in range(len(test[i])-3, 0,-1):
+            temp_result.append((test[i][j], tags[int(temp_tag_index)]))
+            temp_tag_index = backpointer[int(temp_tag_index)][j]
+        temp_result.append((test[i][0], 'START'))
+        temp_result.reverse()
+        result.append(temp_result)
+    return result
+
     raise NotImplementedError("You need to write this part!")
 
 
