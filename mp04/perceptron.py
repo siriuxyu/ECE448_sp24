80567c7b8d0910635ce93f69b709da6c9a16eb8b
Siriuxxx <hengjia.y@outlook.com>
2024-02-05 14:13:51 -0600

    MP04 released

---
+++ mp04/perceptron.py
@@ -0,0 +1,26 @@
+# perceptron.py
+# ---------------
+# Licensing Information:  You are free to use or extend this projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to the University of Illinois at Urbana-Champaign
+#
+# Created by Justin Lizama (jlizama2@illinois.edu) on 10/27/2018
+# Extended by Daniel Gonzales (dsgonza2@illinois.edu) on 3/11/2020
+
+
+
+import numpy as np
+
+def trainPerceptron(train_set, train_labels,  max_iter):
+    #Write code for Mp4
+
+    return W, b
+
+def classifyPerceptron(train_set, train_labels, dev_set, max_iter):
+    #Write code for Mp4
+
+    return []
+
+
+
