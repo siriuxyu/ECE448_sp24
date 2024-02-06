05ae7ddb8159062e1ea54ca9d83b2c0dcb04ac2e
Siriuxxx <hengjia.y@outlook.com>
2024-02-05 20:29:16 -0600

    mp04 finished (seems) 0205

--- mp04/perceptron.py
+++ mp04/perceptron.py
@@ -14,13 +14,31 @@ import numpy as np
 
 def trainPerceptron(train_set, train_labels,  max_iter):
     #Write code for Mp4
+    learning_rate = 0.001
+    W = np.zeros(train_set.shape[1])
+    b = 0
+    for i in range(max_iter):
+        for j in range(train_set.shape[0]):
+            # train_labels is 0 or 1
+            if train_labels[j] == 1 and np.dot(W, train_set[j]) + b <= 0:
+                W += learning_rate * train_set[j]
+                b += learning_rate
+            elif train_labels[j] == 0 and np.dot(W, train_set[j]) + b > 0:
+                W -= learning_rate * train_set[j]
+                b -= learning_rate
 
     return W, b
 
 def classifyPerceptron(train_set, train_labels, dev_set, max_iter):
     #Write code for Mp4
-
-    return []
+    W, b = trainPerceptron(train_set, train_labels, max_iter)
+    result = []
+    for i in range(dev_set.shape[0]):
+        if np.dot(W, dev_set[i]) + b > 0:
+            result.append(1)
+        else:
+            result.append(0)
+    return result
 
 
 
