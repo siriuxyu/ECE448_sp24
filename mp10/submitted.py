fabdb702d6bdcdb02b1975c02f6955ba59805026
Siriux Yu <hengjia.y@outlook.com>
2024-04-14 14:30:36 -0500

    mp10 released

---
+++ mp10/submitted.py
@@ -0,0 +1,52 @@
+'''
+This is the module you'll submit to the autograder.
+
+There are several function definitions, here, that raise RuntimeErrors.  You should replace
+each "raise RuntimeError" line with a line that performs the function specified in the
+function's docstring.
+'''
+import numpy as np
+
+epsilon = 1e-3
+
+def compute_transition(model):
+    '''
+    Parameters:
+    model - the MDP model returned by load_MDP()
+
+    Output:
+    P - An M x N x 4 x M x N numpy array. P[r, c, a, r', c'] is the probability that the agent will move from cell (r, c) to (r', c') if it takes action a, where a is 0 (left), 1 (up), 2 (right), or 3 (down).
+    '''
+    raise RuntimeError("You need to write this part!")
+
+def compute_utility(model, U_current, P):
+    '''
+    Parameters:
+    model - The MDP model returned by load_MDP()
+    U_current - The current utility function, which is an M x N array
+    P - The precomputed transition matrix returned by compute_transition()
+
+    Output:
+    U_next - The updated utility function, which is an M x N array
+    '''
+    raise RuntimeError("You need to write this part!")
+
+def value_iterate(model):
+    '''
+    Parameters:
+    model - The MDP model returned by load_MDP()
+
+    Output:
+    U - The utility function, which is an M x N array
+    '''
+    raise RuntimeError("You need to write this part!")
+
+def policy_evaluation(model):
+    '''
+    Parameters:
+    model - The MDP model returned by load_MDP();
+    
+    Output:
+    U - The converged utility function, which is an M x N array
+    '''
+    raise RuntimeError("You need to write this part!")
